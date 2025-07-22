9ca23ba7515d420b6df7bb4195f1c9623a4c5106
BalajiR2205 <Balaji.Rajarathnam@thomsonreuters.com>
2025-07-22 11:29:54 +0530

    Pytorch basics.

--- pyTfundamentals.py
+++ pyTfundamentals.py
@@ -44,4 +44,21 @@ print(TENSOR)
 
 print(TENSOR.shape)
 
+#Your tensor is essentially a single 3×3 matrix wrapped in an extra dimension. The data structure looks like:
+
 # >>>torch.Size([1, 3, 3]) so 1 represent first [], then 3 represents the elements inside second bracket ["[]"] and 3 represents the elements inside third bracket [["[]"]]
+
+#If you wanted just a 2D tensor without that extra batch dimension, you could write:
+
+TENSOR_2D = torch.tensor([[1,2,3],
+                          [4,5,6],
+                          [7,8,9]])
+print(TENSOR_2D.shape)
+
+#The difference between ndim and shape is
+# ndim - number of dimensions
+# shape - number of elements in the dimentions
+
+tensor_1d = torch.tensor([1,2,3,4])
+print(tensor_1d.ndim) # 1 dimension
+print(tensor_1d.shape) # 4 elements in that one dimension
